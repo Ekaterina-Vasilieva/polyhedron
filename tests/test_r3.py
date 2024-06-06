bf1c34dce733dcdde5a03843e60adc2813ec6601
Ekaterina-Vasilieva <165666931+Ekaterina-Vasilieva@users.noreply.github.com>
2024-06-06 23:22:34 +0300

    Append test_lenght

--- tests/test_r3.py
+++ tests/test_r3.py
@@ -108,3 +108,7 @@ class TestR3(unittest.TestCase):
     def test_cross04(self):
         self.assertEqual(R3ApproxMatcher(self.a.cross(R3(3.0, -2.0, 1.0))),
                          R3(8.0, 8.0, -8.0))
+
+    def test_lenght(self):
+        self.assertEqual(self.a.lenght(), sqrt(14.0))
+
